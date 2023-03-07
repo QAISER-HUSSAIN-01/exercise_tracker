c9bbbe2e620727ba5c0ce1029494bb1c6f6a9e6f
Qaiser Hussain <hussaqai@corp.contoursoftware.net>
2023-03-07 11:32:37 +0500

    bug testing

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,16 @@
-import { NextResponse } from 'next/server'
-import { NextRequest } from 'next/server'
-import {url} from './src/utils/url'
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import { url } from "./src/utils/url";
 export const config = {
-    matcher: ['/dashboard/:path*']
-}
-
+  matcher: ["/dashboard/:path*"],
+};
 
 export function middleware(req: NextRequest, res: NextResponse) {
-    const token = req.cookies.get('token')?.value
-    if (token) {
-        NextResponse.redirect(`${url}dashboard`);
-        return NextResponse.next();
-    } else {
-        return NextResponse.redirect(`${url}signin`);
-    }
+  const token = req.cookies.get("token")?.value;
+  if (token) {
+    NextResponse.redirect(`${url}dashboard`);
+  } else {
+    return NextResponse.redirect(`${url}signin`);
+  }
+  NextResponse.next();
 }
