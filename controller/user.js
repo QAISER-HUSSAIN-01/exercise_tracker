f9f8a06feb959d59bb5503100da4986cd51b17da
QaiserHussain <qaiserh844@gmail.com>
2023-03-14 11:18:06 +0500

    work on profile update controller

--- controller/user.js
+++ controller/user.js
@@ -1,9 +1,9 @@
 import User from "../database/models/users";
-
+import bcrypt from 'bcrypt'
 export const getAllUsers = async (req, res) => {
   try {
-    const users = await User.find().sort({createdAt:-1});
-    if (!users) { return res.status(404).json({ status: false, message: 'users not found'}) }
+    const users = await User.find().sort({ createdAt: -1 });
+    if (!users) { return res.status(404).json({ status: false, message: 'users not found' }) }
     res.status(200).json({ success: true, data: users });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
@@ -11,10 +11,10 @@ export const getAllUsers = async (req, res) => {
 };
 
 export const getUser = async (req, res) => {
-  const {id} = req.query;
+  const { id } = req.query;
   try {
     const user = await User.findById(id);
-    if (!user) { return res.status(404).json({ status: false, message: 'user not found'}) }
+    if (!user) { return res.status(404).json({ status: false, message: 'user not found' }) }
     res.status(200).json({ success: true, data: user });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
@@ -22,8 +22,8 @@ export const getUser = async (req, res) => {
 };
 
 export const addUser = async (req, res) => {
-  const { name , email , password } = req.body;
-  if (!name && !email && !password ) {
+  const { name, email, password } = req.body;
+  if (!name && !email && !password) {
     return res
       .status(404)
       .json({ success: false, message: "please fill all fields" });
@@ -43,18 +43,39 @@ export const addUser = async (req, res) => {
 
 export const deleteUser = async (req, res) => {
   const { id } = req.query;
-    try {
-        const user = await User.findByIdAndDelete(id);
-        if (!user) { return res.status(404).json({ status: false, message: 'user not found'}) }
-        return res.status(200).json({ success: true, message: 'user deleted'});
-    } catch (err) { res.status(500).json({ success: false, message: err.name }) }
+  try {
+    const user = await User.findByIdAndDelete(id);
+    if (!user) { return res.status(404).json({ status: false, message: 'user not found' }) }
+    return res.status(200).json({ success: true, message: 'user deleted' });
+  } catch (err) { res.status(500).json({ success: false, message: err.message }) }
 };
 
 export const updateUser = async (req, res) => {
   const { id } = req.query;
-    try {
-        const user = await User.findByIdAndUpdate(id, req.body, { new: true });
-        if (!user) { return res.status(404).json({ status: false, message: 'user not updated'})}
-        return res.status(200).json({ success: true, message: 'user updated', data: user });
-    } catch (err) { res.status(500).json({ success: false, message: err.name }) }
+  const { username, email, password, newpassword } = req.body;
+  console.log('check filed');
+ console.log(req.body);
+  if (!username && !email && !password) {
+    return res
+      .status(404)
+      .json({ success: false, message: "please fill all fields" });
+  }
+  try {
+    console.log('try run');
+    const exist = User.findById(id);
+    console.log(exist);
+    if (!exist) { return res.status(404).json({ status: false, message: 'user not found' }) }
+    const compared = bcrypt.compare(password, exist.password)
+    if (!compared) { return res.status(400).json({ status: false, message: 'password is incorrect' }) }
+    if (newpassword) {
+      const user = await User.findByIdAndUpdate(id, { username: username, email: email, password: newpassword }, { new: true });
+      if (!user) { return res.status(404).json({ status: false, message: 'user not updated' }) }
+      return res.status(200).json({ success: true, message: 'user updated', data: user });
+    } else {
+      const user = await User.findByIdAndUpdate(id, {username: username, email: email} , { new: true });
+      if (!user) { return res.status(404).json({ status: false, message: 'user not updated' }) }
+      return res.status(200).json({ success: true, message: 'user updated', data: user });
+    }
+
+  } catch (err) { res.status(500).json({ success: false, message: err.message }) }
 };
--- src/components/updateForm/index.jsx
+++ src/components/updateForm/index.jsx
@@ -4,31 +4,47 @@ import { Button, InputAdornment, TextField } from '@mui/material'
 import {FormContainer,FormHeading,FormFields,FormActions,ActionMessage} from '../styledForm'
 import Link from 'next/link'
 import useNotify from '../../hooks/useNotify';
+import axios from 'axios'
+import {url} from '../../utils/url'
+
 export default function UpdateForm() {
-  const {successMessage} = useNotify();
-  const [data,setData] = useState({id:'',image:'',username: '', email: "", password:''});
+  const {successMessage,errorMessage} = useNotify();
+  const [user,setUser] = useState({id:'',username: '', email: "", password:'',newpassword:''});
   const handleChange = (e)=>{
     e.preventDefault();
-    setData({...data,[e.target.name]:e.target.value});
+    setUser({...user,[e.target.name]:e.target.value});
   }
-  const handleSubmit = ()=>{
-    console.log(data);
-    successMessage('Updated Successfully')
+  const handleSubmit = async()=>{
+    console.log(user);
+    try {
+      const { data } = await axios.patch(`${url}api/user/${user.id}`, user)
+      console.log(data);
+      if (data.success) {
+          localStorage.setItem('user',JSON.stringify(data.data))
+          successMessage('User updated')
+          await router.push('/dashboard/profile')
+      } else {
+          errorMessage('User Not Updated')
+      }
+  } catch (error) {
+      errorMessage(error.message)
   }
+}
 
   useEffect(() => {
     const userDetail = JSON.parse(localStorage.getItem('user'));
     const {_id,username,email} = userDetail;
-    setData({id:_id,image:'',username:username,email:email,password:''})
+    setUser({id:_id,username:username,email:email,password:''})
   }, [])
   
   return (
     <FormContainer>
       <FormHeading>Update Profile</FormHeading>
       <FormFields>
-      <TextField fullWidth placeholder='Username' name='username' value={data.username} onChange={handleChange} InputProps={{ endAdornment: <InputAdornment position='end'><MdPerson /></InputAdornment> }} />
-        <TextField fullWidth placeholder='Email' name='email' value={data.email} onChange={handleChange} InputProps={{ endAdornment: <InputAdornment position='end'><MdMail /></InputAdornment> }} />
-        <TextField fullWidth placeholder='Password' name='password' value={data.password} onChange={handleChange} InputProps={{ endAdornment: <InputAdornment position='end'><MdKey /></InputAdornment> }} />
+      <TextField fullWidth placeholder='Username' name='username' value={user.username} onChange={handleChange} InputProps={{ endAdornment: <InputAdornment position='end'><MdPerson /></InputAdornment> }} />
+        <TextField fullWidth placeholder='Email' name='email' value={user.email} onChange={handleChange} InputProps={{ endAdornment: <InputAdornment position='end'><MdMail /></InputAdornment> }} />
+        <TextField fullWidth placeholder='Old Password' name='password' value={user.password} onChange={handleChange} InputProps={{ endAdornment: <InputAdornment position='end'><MdKey /></InputAdornment> }} />
+        <TextField fullWidth placeholder='New Password' name='newpassword' value={user.newpassword} onChange={handleChange} InputProps={{ endAdornment: <InputAdornment position='end'><MdKey /></InputAdornment> }} />
       </FormFields>
       <FormActions>
         <ActionMessage variant='caption'>Go to User Profile <Link href={'/dashboard/profile'} style={{ color: 'blue', borderBottom: '1px solid blue' }}>Profile</Link>  </ActionMessage>
