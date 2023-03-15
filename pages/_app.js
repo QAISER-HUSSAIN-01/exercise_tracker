7216b497d29c1d7cecb0344de3ee9a931d93f541
QaiserHussain <qaiserh844@gmail.com>
2023-03-15 10:05:34 +0500

    add spinner

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,35 @@
 import '../styles/globals.css';
-import { ThemeProvider, CssBaseline } from '@mui/material';
+import { ThemeProvider, CssBaseline, Box } from '@mui/material';
 import theme from '../src/theme';
 import Landing from '../src/layouts/landingLayout/Landing';
-import {getCookie} from 'cookies-next';
 import { SnackbarProvider } from 'notistack'
-// import { CookiesProvider } from 'react-cookie'
+import Router from 'next/router'
+import { useEffect, useState } from 'react';
+import {CircularProgress} from '@mui/material'
 export default function App({ Component, pageProps }) {
-  
-  const renderLayout =  Component.getLayout || function (page) { return <Landing>{page}</Landing> }
- 
+  const [isLoading, setIsLoading] = useState(false);
+
+  const renderLayout = Component.getLayout || function (page) { return <Landing>{page}</Landing> }
+  useEffect(() => {
+    const start = ()=>{setIsLoading(true)}
+    const close = ()=>{setIsLoading(false)}
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', close);
+    return ()=>{
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', close);
+    }
+  }, [])
+
   return (
     // <CookiesProvider>
-    
-      <SnackbarProvider autoHideDuration={3000} anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          {renderLayout(<Component {...pageProps}/>)}
-        </ThemeProvider>
-      </SnackbarProvider>
+
+    <SnackbarProvider autoHideDuration={3000} anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {renderLayout(isLoading ? <Box sx={{width:'100%',height:'80vh',display:'flex',justifyContent:'center',alignItems:'center'}}><CircularProgress /></Box> : <Component {...pageProps} />)}
+      </ThemeProvider>
+    </SnackbarProvider>
     // </CookiesProvider>
   )
 }
